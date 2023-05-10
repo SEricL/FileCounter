3c526ea6850b77baee1670b85d5d63698f957f4b
SEricL <eric_lan_2001@hotmail.com>
2023-05-10 19:02:35 -0400

    Update Counter.py

--- Counter.py
+++ Counter.py
@@ -2,13 +2,9 @@ import os
 import os.path
 import re
 
+cont = True
 
-print("Enter the path you want to search:")
-path = input()
-
-if len(path) < 3:
-    # Default path is directory this file is in
-    path = os.path.dirname(__file__)
+#TODO: Change to non-cmd based (probably tkinter)
 
 def getBytes(name: str) -> int:
     # Returns size of file in bytes. Divide by 1024 for each step up (MB, GB, etc.)
@@ -16,54 +12,62 @@ def getBytes(name: str) -> int:
         return os.stat(path + '\\' + name).st_size
     return os.stat(path + '\\' + name[0]).st_size
 
+
 def bytePrint(bytes: int, mag: str) -> str:
     mag = mag.upper()
     match mag:
+        case "KB":
+            return str(bytes / 1024) + " KB"
         case "MB":
-            return bytes / pow(1024,2)
+            return str(bytes / pow(1024, 2)) + " MB"
         case "GB":
-            return bytes / pow(1024,3)
+            return str(bytes / pow(1024, 3)) + " GB"
         case "TB":
-            return bytes / pow(1024,4)
+            return str(bytes / pow(1024, 4)) + " TB"
         case _:
-            return bytes
+            return str(bytes) + " Bytes"
+
 
 def getFiles(criteria: str, path):
     pattern = re.compile(criteria, re.IGNORECASE)
     # Gets all the files in directory
     fileNames = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    # Pick matching only
     fileNames = [f for f in fileNames if re.match(pattern, f)]
     # Converts initial list to 2d array with byte size in second slot.
     files = []
     for i in range(len(fileNames)):
         files.append([fileNames[i], getBytes(fileNames[i])])
-    files.sort(key=lambda x: x[0])
+    files.sort(key=lambda x: x[0])  # Sort by name. (To use index 1
     return files
 
-print("The following uses python's Re Library")
-print("Enter criteria:")
-criteria = input()
-files = getFiles(criteria, path)
+allFiles = []
+queries = []
 
-filesCopy = files.copy()
-sizes = []
-totalSize = 0  # Wil be kept in bytes, but converted when printed
+while cont:
+    print("Enter the path you want to search:(exit to end)")
+    path = input()
 
-while len(filesCopy) > 0:
-    # File group criteria
-    current = filesCopy[0][0]
-    currentSize = 0
-    while len(filesCopy) > 0 and current in filesCopy[0][0]:
-        if current in filesCopy[0][0]:
-            currentSize += filesCopy[0][1]
-            filesCopy.pop(0)
-    totalSize += currentSize
-    currentSize /= (1024 * 1024)  # Outputs MB. (If less than 1, puts 0)
-    sizes.append([current, int(currentSize)])
+    if len(path) < 3:
+        # Default path is directory this file is in
+        path = os.path.dirname(__file__)
+    if path.lower() == "exit":
+        cont = False
+        break
 
+    print("The following uses python's Re Library")
+    print("Enter criteria:")
+    criteria = input()
+    files = getFiles(criteria, path)
 
-# print(sizes)
-sizes.sort(key=lambda x: x[1])
-sizes.reverse()
-for a, s in sizes:
-    print(a, s, 'MB')
+    allFiles += files
+    totalSize = 0  # Wil be kept in bytes, but converted when printed
+
+    for file in files:
+        totalSize += file[1]
+        print(file[0], bytePrint(file[1], "KB"))
+
+    # Store previous queries
+    queries += [path, criteria]
+
+print("Total size:", totalSize)  #TODO: Change to adapt to closest size (kb,mb, etc.)
